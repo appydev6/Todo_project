4f851937244df8dd6187c7c50f588fd40172f82f
appydev6 <ashwinpahade97@gmail.com>
2025-12-28 13:28:15 +0530

    settings and requirements done

--- Todo_project/Todo_project/settings.py
+++ Todo_project/Todo_project/settings.py
@@ -11,21 +11,38 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+import dj_database_url
+
+env = environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3qodd1sb=gr5*q2tiwyy%^2^)$fiqqryt(qo#9quu610)!mil'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+# MODE
+MODE = os.environ.get("MODE")
+
+# DB_STRING
+DB_STRING = os.environ.get('DB_STRING')
+
+IS_DEV_ENV = MODE == 'dev'
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -74,12 +91,17 @@ WSGI_APPLICATION = 'Todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if IS_DEV_ENV:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(DB_STRING)
+    }
 
 
 # Password validation
