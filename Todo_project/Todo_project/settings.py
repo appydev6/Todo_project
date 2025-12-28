4e3b8f538f62b9d9796f674a8c4c4f4af1cda592
appydev6 <ashwinpahade97@gmail.com>
2025-12-29 01:06:05 +0530

    debug updated

--- Todo_project/Todo_project/settings.py
+++ Todo_project/Todo_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == 'on'
 
 # MODE
 MODE = os.environ.get("MODE")
